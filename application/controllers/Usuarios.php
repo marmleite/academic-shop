52f95d9c93e17ec4d480c92d0b58fab31a67478b
marcelo <marmleite@hotmail.com>
2019-06-13 21:47:23 -0300

    adiciona controller usuario e funcao view

---
+++ application/controllers/Usuarios.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Usuarios extends CI_Controller {
+
+
+	public function view($page = 'home')
+	{
+		# code...
+	}
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Usuarios.php */
